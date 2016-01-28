5bce67eb141c138f99eb2e81f4af191cbe9ae426
gucheng <guchengf@gmail.com>
2016-01-28 19:23:48 +0800

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,43 @@
+/**
+ * Created by gucheng on 1/21/16.
+ */
+import {Component} from 'angular2/core'
+import {ContenteditableModel} from "./shared/directives/contenteditable-model";
+import LeancloudConfig from './leancloud-config'
+
+declare var AV:any;
+
+AV.initialize(LeancloudConfig.AppId, LeancloudConfig.AppKey);
+
+const Diary = AV.Object.extend('Diary');
+
+@Component({
+  selector: 'diary',
+  templateUrl: 'app/app.html',
+  directives: [ContenteditableModel]
+})
+
+export class AppComponent {
+  public title = 'Journal';
+
+  public content = '';
+
+  diary = new Diary();
+
+  submit() {
+    if (!this.content) {
+      console.log('content-empty');
+      return;
+    }
+    this.diary.save({
+        pub_timestamp: new Date(),
+        content: this.content
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+}
